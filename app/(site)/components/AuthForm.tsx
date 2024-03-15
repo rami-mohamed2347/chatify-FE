6620641ec1e962da7139a33ed89dcc635143ac28
rami-mohamed2347 <rami.mohamed2347@gmail.com>
2024-03-15 19:34:20 +0200

    Created inital Auth Forms
    Created the inital logic for the nexauth forms

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Input from "@/app/components/inputs/Input";
+import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthFrom = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      // axios register
+    }
+
+    if (variant === "LOGIN") {
+      // nextauth signin
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth social sign in
+  };
+
+  return (
+    <div
+      className="
+  mt-8
+  sm:mx-auto
+  sm:w-full
+  sm:max-w-md
+  "
+    >
+      <div
+        className="
+     bg-white
+     px-4
+     py-8
+     shadow
+     sm:rounded-lg
+     sm:px-10
+    "
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+             absolute
+             inset-0
+             flex
+              items-center
+            "
+            >
+              <div className="w-full border-t boder-gray-300" />
+            </div>
+            <div className=" relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthFrom;
---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,11 @@
+const AuthSocialButton = () => {
+    return ( 
+        <div>
+            Social Button
+
+            <div></div>
+        </div>
+     );
+}
+ 
+export default AuthSocialButton;
